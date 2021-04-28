ecb7eb639392ee9ef49112061bfd9e22648e25bf
manexonmorin <62068818+manexonmorin@users.noreply.github.com>
2021-04-28 19:09:35 -0400

    ajout du sidebar dans mon site

--- header.php
+++ header.php
@@ -20,7 +20,9 @@
 	<?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
+<body <?php body_class( is_front_page() || is_category() ? 'no-sidebar': ''); ?>>
+
+
 <?php wp_body_open(); ?>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'theme4w4' ); ?></a>
